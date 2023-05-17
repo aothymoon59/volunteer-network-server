f76edf2df1b1d3f809964e74bfc129e64e433e4e
aothymoon59 <mmoon8745@gmail.com>
2023-05-17 20:42:40 +0600

    first commit

---
+++ index.js
@@ -0,0 +1,62 @@
+const express = require("express");
+const cors = require("cors");
+require("dotenv").config();
+const { MongoClient, ServerApiVersion } = require("mongodb");
+const app = express();
+const port = process.env.PORT || 5000;
+
+// middleware
+app.use(cors());
+app.use(express.json());
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.tgrk550.mongodb.net/?retryWrites=true&w=majority`;
+
+// Create a MongoClient with a MongoClientOptions object to set the Stable API version
+const client = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  },
+});
+
+async function run() {
+  try {
+    // Connect the client to the server	(optional starting in v4.7)
+    // await client.connect();
+
+    const registeredVolunteers = client
+      .db("volunteerNetwork")
+      .collection("registeredVolunteers");
+
+    app.get("/volunteers", async (req, res) => {
+      const cursor = registeredVolunteers.find();
+      const result = await cursor.toArray();
+      res.send(result);
+    });
+
+    app.post("/volunteers", async (req, res) => {
+      const volunteer = req.body;
+      const result = await registeredVolunteers.insertOne(volunteer);
+      res.send(result);
+    });
+
+    // Send a ping to confirm a successful connection
+    await client.db("admin").command({ ping: 1 });
+    console.log(
+      "Pinged your deployment. You successfully connected to MongoDB!"
+    );
+  } finally {
+    // Ensures that the client will close when you finish/error
+    // await client.close();
+  }
+}
+run().catch(console.dir);
+
+app.get("/", (req, res) => {
+  res.send("Volunteer network is active");
+});
+
+app.listen(port, () => {
+  console.log(`Volunteer network is active on port: ${port}`);
+});
